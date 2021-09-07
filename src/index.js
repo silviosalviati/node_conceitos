a13db683e8ca71a76058bc9e4143a5ae00fc0a3d
silvio salviati junior <salviati@Dev.localdomain>
2021-09-07 03:16:57 -0300

    Rota de usuarios completada

--- src/index.js
+++ src/index.js
@@ -1,24 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user)=> user.username === username);
+  if(!user){
+    return response.status(400).json({error:"User not exists!"});
+  }
+  request.user = user;
+  return next();
+
+
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { name,username } = request.body;
+  const userAlreadyExists = users.some((user)=>user.username === username);
+
+  if(userAlreadyExists){
+    return response.status(401).json({
+      error:"User already exists!"
+    });
+  }
+  users.push({
+    id: uuidv4(), 
+    name,
+    username, 
+    todos: []
+  });
+  return response.status(201).send();
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.get('/todos',checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 

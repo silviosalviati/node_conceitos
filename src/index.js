775b01e6c88aefd2ed92bc5695e3654f58fd81bd
silvio salviati junior <salviati@Dev.localdomain>
2021-09-07 20:45:59 -0300

    Rota patch finalizada

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ app.use(express.json());
  const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  // Complete aqui - concluido
   const { username } = request.headers;
   const user = users.find((user)=> user.username === username);
   if(!user){
-    return response.status(400).json({error:"User not exists!"});
+    return response.status(404).json({error:"User not exists!"});
   }
   request.user = user;
   return next();
@@ -24,12 +24,12 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  // Complete aqui - concluido
   const { name,username } = request.body;
   const userAlreadyExists = users.some((user)=>user.username === username);
 
   if(userAlreadyExists){
-    return response.status(401).json({
+    return response.status(400).json({
       error:"User already exists!"
     });
   }
@@ -39,23 +39,67 @@ app.post('/users', (request, response) => {
     username, 
     todos: []
   });
-  return response.status(201).send();
+  return response.status(201).json(users);
 });
 
 app.get('/todos',checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // Complete aqui - concluido
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // Complete aqui - concluido
+  const { user } = request;
+  const { title,deadline } = request.body;
+
+  const todoOperation ={
+    id: uuidv4(),
+	  title,
+	  done: false, 
+	  deadline: new Date(deadline),
+	  created_at: new Date()
+  }
+  user.todos.push(todoOperation);
+  return response.status(201).json(todoOperation);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // Complete aqui - Concluido
+  const { user } = request;
+  const { title, deadline} = request.body;
+  const { id } = request.params;
+
+  const userTodos = user.todos.find((todo)=>todo.id === id);
+    
+  if(!userTodos){
+    return response.status(404).json({
+      error:"Todo not found!"
+    });
+  }
+  userTodos.title = title;
+  userTodos.deadline = new Date(deadline);
+
+  return response.status(201).json(userTodos);
+  
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoDone = user.todos.find((done)=> done.id === id);
+
+  if(!todoDone){
+    return response.status().json({
+      error:"Todo not exists!"
+    });
+  }
+  todoDone.done = true;
+
+  return response.status(201).json(todoDone);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
